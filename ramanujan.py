0bdc8e581157430109158700fbc8ea64f1117ea5
gmittal <gautam@mittal.net>
2016-04-30 19:01:04 -0700

    woah, svms are cool

--- ramanujan.py
+++ ramanujan.py
@@ -1,6 +1,9 @@
 import tensorflow.contrib.learn as skflow
 import matplotlib.pyplot as plt
 import math
+import numpy as np
+from sklearn import preprocessing
+from sklearn.svm import SVR
 
 # True partition function using recursive algorithm
 def partition(n):
@@ -21,7 +24,7 @@ def main():
     print(approximate_partition(10))
 
     partitionX = []
-    for x in range(1, 22):
+    for x in range(1, 20):
         partitionX.append(x)
     actualPartitionY = []
     approximatePartitionY = []
@@ -29,9 +32,32 @@ def main():
         actualPartitionY.append(len(partition(partitionX[x])))
         approximatePartitionY.append(approximate_partition(partitionX[x]))
 
+    regressor = SVR(kernel='rbf', C=1e3, gamma=0.1)
+    trainingX = []
+    for x in range(0, len(partitionX)):
+        tmp = []
+        tmp.append(partitionX[x])
+        trainingX.append(tmp)
+    trainingY = []
+    for x in range(0, len(partitionX)):
+        tmp = []
+        tmp.append(actualPartitionY[x])
+        trainingY.append(tmp)
+
+    print(trainingX)
+    print(trainingY)
+    regressor.fit(np.asarray(trainingX, dtype=np.int32), np.asarray(trainingY, dtype=np.int32))
+    print(regressor.predict(np.asarray([10], dtype=np.int32)))
+
+    estimatedRegressorY = []
+    for x in range(0, len(partitionX)):
+        tmp = []
+        tmp.append(partitionX[x])
+        estimatedRegressorY.append(regressor.predict(np.asarray(tmp, dtype=np.int32))[0])
 
     plt.plot(partitionX, actualPartitionY)
     plt.plot(partitionX, approximatePartitionY)
+    plt.plot(partitionX, estimatedRegressorY)
 
     plt.show()
 
